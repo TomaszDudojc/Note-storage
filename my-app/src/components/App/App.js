8f100b7c90972b1e3786f903f874b20c59c921e1
Tomasz Dudojć <tomek.dudojc@gmail.com>
2024-10-16 15:42:29 +0200

    store token in sessionStorage

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
+import useToken from './useToken';
 
 function App() {
-  const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
   if(!token) {
     return <Login setToken={setToken} />
